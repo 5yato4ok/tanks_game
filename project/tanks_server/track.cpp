936f3b6707b21fb7e226cef8c15b99bce12b40bd
Batman <pyatochok37@gmail.com>
2018-07-13 10:03:52 +0300

    Added some tracks method

--- project/tanks_server/track.cpp
+++ project/tanks_server/track.cpp
@@ -5,17 +5,27 @@ Track_mngr::Track_mngr(Ui_MainWindow* gui_, ArduinoSender& ard_mngr_)
   :gui(gui_),ard_mngr(ard_mngr_) {
   current_x = 0;
   current_y = 0;
+  current_tracks.left_track.is_right = false;
+  current_tracks.left_track.velocity = false;
+  current_tracks.left_track.up_down = direction::NONE;
+  current_tracks.right_track.is_right = false;
+  current_tracks.right_track.velocity = false;
+  current_tracks.right_track.up_down = direction::NONE;
 }
 
-std::string Track_mngr::form_arduino_packet(const Track_desc& left_track,
-  const Track_desc& right_track) {
-  return std::string();
-}
 //M000111 Ц управление гусеницами танка, где :
 //  1 цифра Ц направление движени€ левой гусеницы(0 - вперед, 1 Ц назад)
 //  2 - 3 цифры Ц мощность в процентах от 00 до 99 поданна€ на левую гусеницу танка
 //  4 цифра Ц направление движени€ правой гусеницы
 //  5 - 6 цифра Ц мощность в процентах правой гусеницы
+
+std::string Track_mngr::form_arduino_packet(const Tank_Tracks& tracks_descr) {
+  std::string command = "M" + std::to_string(int(tracks_descr.left_track.up_down)) +
+    std::to_string(tracks_descr.left_track.velocity) +
+    std::to_string(int(tracks_descr.right_track.up_down)) +
+    std::to_string(tracks_descr.right_track.velocity);
+  return command;
+}
 
 bool Track_mngr::is_left_turn(const TankAction& action) {
   return true;
@@ -25,24 +35,66 @@ bool Track_mngr::is_right_turn(const TankAction& action) {
   return false;
 }
 
+track_move Track_mngr::get_track_move_kind(const TankAction& action) {
+  return track_move();
+}
+
+bool Track_mngr::is_first_launch() {
+  return current_tracks.left_track.velocity == 0 && 
+    current_tracks.right_track.velocity == 0;
+}
+
+tank_status Track_mngr::send_action_sequence(Tank_Tracks& tracks_descr) {
+  Track_desc max_track;
+  Track_desc min_track;
+  tank_status result;
+  if (tracks_descr.left_track == tracks_descr.right_track) {
+    max_track = min_track = tracks_descr.left_track;
+  } else if (tracks_descr.left_track < tracks_descr.right_track) {
+    max_track = tracks_descr.right_track;
+    min_track = tracks_descr.left_track;
+  } else {
+    max_track = tracks_descr.left_track;
+    min_track = tracks_descr.right_track;
+  }
+  for (int i = 0; i < max_track.velocity; i++) {
+    Tank_Tracks tmp_descr;
+    tmp_descr.left_track.up_down = tracks_descr.left_track.up_down;
+    tmp_descr.right_track.up_down = tracks_descr.right_track.up_down;
+    if (max_track.is_right) {
+      tmp_descr.right_track.velocity = i;
+    } else {
+      tmp_descr.left_track.velocity = i;
+    }
+    if (min_track.is_right && tmp_descr.right_track < min_track) {
+      tmp_descr.right_track.velocity = i;
+    } else if (!min_track.is_right && tmp_descr.right_track < min_track){
+      tmp_descr.left_track.velocity = i;
+    }
+    result = ard_mngr.SendAction(form_arduino_packet(tracks_descr));
+  }
+  return result;
+}
+
 tank_status Track_mngr::ManageAction(TankAction& action) {
-  Track_desc descr_left;
-  Track_desc descr_right;
+  Tank_Tracks tracks_descr;
   switch (get_track_move_kind(action)) {
-  case track_move::CONTINUE_MOVE : { 
-    break; 
-  }
-  case track_move::DIRECT_MOVE: { 
-    break; 
-  }
-  case track_move::ROTATE_MOVE: { 
-    break; 
-  }
+  case track_move::CONTINUE_MOVE:
+    tracks_descr = current_tracks;
+    break;
+  case track_move::DIRECT_MOVE:
+    tracks_descr = get_direct_descr(action);
+    break;
+  case track_move::ROTATE_MOVE:
+    tracks_descr = get_rotate_descr(action);
+    break;
   default:
     return tank_status::OPERATION_FAILED;
   }
-  ard_mngr.SendAction(form_arduino_packet(descr_left,descr_right));
-  return tank_status();
+  if (!is_first_launch()) {
+    return ard_mngr.SendAction(form_arduino_packet(tracks_descr));
+  } 
+  return send_action_sequence(tracks_descr);
 }
 
 direction Track_mngr::get_direction(const TankAction& descr) {
@@ -54,7 +106,6 @@ int8_t get_common_velocity(const TankAction& descr) {
 }
 
 int8_t Track_mngr::get_delta_velocity(const TankAction& descr) {
-
   return int8_t();
 }
 }//namespace tank
--- project/tanks_server/track.h
+++ project/tanks_server/track.h
@@ -7,9 +7,24 @@
 #include <stdint.h>
 //TODO: describe limit
 namespace tank {
-struct Track_desc {
+
+class Track_desc {
+public:
+  Track_desc() { velocity = 0; up_down = direction(0); };
+  bool operator <(const Track_desc& rhs) {
+    return velocity < rhs.velocity;
+  }
+  bool operator == (const Track_desc& rhs) {
+    return velocity == rhs.velocity;
+  }
   int8_t velocity;
   direction up_down;
+  bool is_right;
+};
+
+struct Tank_Tracks {
+  Track_desc left_track;
+  Track_desc right_track;
 };
 
 enum class track_move : int32_t {CONTINUE_MOVE,DIRECT_MOVE,ROTATE_MOVE, LAST};
@@ -22,26 +37,22 @@ class Track_mngr:public QObject {
  private:
   Ui_MainWindow* gui;
   ArduinoSender& ard_mngr;
-  track_move get_tack_move_kind(const TankAction& action);
-  bool is_continue_move(const TankAction& action);
-  bool is_rotate_move(const TankAction& action);
-  bool is_direct_move(const TankAction& action);
-
+  track_move get_track_move_kind(const TankAction& action);
   bool is_left_turn(const TankAction& action);
   bool is_right_turn(const TankAction& action);
   bool is_up_move(const TankAction& action);
   bool is_down_move(const TankAction& action);
-
-  direction get_direction(const TankAction& descr);
-  std::string form_arduino_packet(const Track_desc& left_track, 
-    const Track_desc& right_track);
-  
   int8_t get_delta_velocity(const TankAction& descr);
   int8_t get_common_velocity(const TankAction& descr);
+  direction get_direction(const TankAction& descr);
+  Tank_Tracks get_direct_descr(const TankAction& descr);
+  Tank_Tracks get_rotate_descr(const TankAction& descr);
+  std::string form_arduino_packet(const Tank_Tracks& tracks_descr);
+  tank_status send_action_sequence(Tank_Tracks& tracks_descr);
+  bool is_first_launch();
+  Tank_Tracks current_tracks;
   double current_x;
   double current_y;
-  Track_desc current_left_track;
-  Track_desc current_right_track;
   const int8_t max_speed = 100;
   const int8_t velocity_step = 1;
 };
