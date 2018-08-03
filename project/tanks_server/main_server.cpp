72efbb5cc936ab1d0413a961c79a749bcb32ff83
Batman <pyatochok37@gmail.com>
2018-08-03 16:39:40 +0300

    Some little filter changing

--- project/tanks_server/main_server.cpp
+++ project/tanks_server/main_server.cpp
@@ -4,14 +4,10 @@
 #include <thread>
 int main(int argc, char *argv[]) {
   QApplication a(argc, argv);
-  //tank::TankMngr tank;
   QMainWindow window;
   Ui_MainWindow gui;
-  gui.setupUi(&window);
-  //std::thread gui_thread = std::thread(&Ui_MainWindow::setupUi, &window);
-  
+  gui.setupUi(&window); 
   tank::TankMngr tk(&gui);
   window.showNormal();
-  //Here goes a server fun
   return a.exec();
 }
